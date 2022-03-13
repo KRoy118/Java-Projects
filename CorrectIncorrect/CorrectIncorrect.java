241103f758880d2583ee31c35fde924100cafb49
Katha Roy <55144881+KRoy118@users.noreply.github.com>
2022-03-13 11:13:59 +0530

    Updated

--- CorrectIncorrect/CorrectIncorrect.java
+++ CorrectIncorrect/CorrectIncorrect.java
@@ -7,27 +7,27 @@ public class CorrectIncorrect
     {int c=0;
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
-        //int diffofLastFirst=Character.getNumericValue(str.charAt(str.length()-1))-Character.getNumericValue(str.charAt(0));
-        System.out.println(diffofLastFirst);
+        int diffofLastFirst=Character.getNumericValue(str.charAt(str.length()-1))-Character.getNumericValue(str.charAt(0));
+        //System.out.println(diffofLastFirst);
         if(diffofLastFirst==1||diffofLastFirst==-1)
-       {   c=1;
+         c=1;
            for(int i=0;i<str.length()-1;i++)
           {
-            char ch=str.charAt(i);
-            //char ch1=str.charAt(i-1);
-            int diff=Character.getNumericValue(ch)-Character.getNumericValue(ch-1);
-           
+            char ch=str.charAt(i);//System.out.println(ch);
+            char ch1=str.charAt(i+1);
+            int diff=Character.getNumericValue(ch)-Character.getNumericValue(ch1);
+            System.out.println(diff);
             if(diff==1||diff==-1)
             {
                 c++;
             }
 
            }
-        }
+    
         if(c==str.length())
-        System.out.println("correct");
+        System.out.println(c+""+"correct");
         else
-        System.out.println("incorrect");
+        System.out.println(c+""+"incorrect");
     }  
 
 }
