db322c220aa699325c97dbced8f2e4a6ad8617eb
Katha Roy <55144881+KRoy118@users.noreply.github.com>
2022-03-13 11:57:27 +0530

    Updated

--- CyclicRotationOfArraySplitPart/Main.java
+++ CyclicRotationOfArraySplitPart/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.*;
 class Main
 {//Spaces between outputs added for clarity.It wasn't asked in question
     public static void main(String[]args)
@@ -13,17 +14,17 @@ class Main
         int  n=sc.nextInt();int br[]=new int[100];int s1=0,s2=0;
        // int cr[]=new int[100];
         
-        for(i=l;i>n-1;i--)
+        for(i=l-1;i>Math.abs((l-1)-n);i--)
        { s1=j;br[j]=ar[i];j++;}
        
        //for(i=0;i<n-1;i++)
       //{ s2=k;cr[k]=ar[i];k++;}
       System.out.println();
-        for(i=j-1;i>0;i--)
+        for(i=j-1;i>=0;i--)
       System.out.println(br[i]);
       System.out.println();
       
-      for(i=0;i<n;i++)
+      for(i=0;i<=Math.abs((l-1)-n);i++)
       System.out.println(ar[i]);
     
       
