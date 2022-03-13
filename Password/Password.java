83c9d353cd91a2c8accbcc9bfbf5ccf68826747a
Katha Roy <55144881+KRoy118@users.noreply.github.com>
2022-03-13 12:06:29 +0530

    Updt

--- Password/Password.java
+++ Password/Password.java
@@ -4,22 +4,7 @@ import java.util.Scanner;
 
 public class Password
 {
-    /*static String Result(String s)
-    {
-        String alphabets="",specialCharacter="";
-        for(int i=0;i<s.length();i++)
-        {
-            if((s.charAt(i)>='A'&& s.charAt(i)<='Z') || (s.charAt(i)>='a'&& s.charAt(i)<='z'))
-            {
-            	alphsabets+=s.charAt(i);
-            }
-            else if(!(s.charAt(i)>='0'&& s.charAt(i)<='9'))
-            {
-            	specialCharacter+=s.charAt(i);
-            }
-        }
-        return alphabets+specialCharacter;
-    }*/
+    
  public static void main(String[] args) 
  {
   Scanner sc=new Scanner(System.in);
